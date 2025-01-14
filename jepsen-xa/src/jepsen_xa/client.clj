148f40cf63e962b3ed5b25010721abf4ed763963
Ryotaro <ryotaro612.ooo@gmail.com>
2025-01-15 03:19:25 +0900

    update

--- jepsen-xa/src/jepsen_xa/client.clj
+++ jepsen-xa/src/jepsen_xa/client.clj
@@ -1,6 +1,7 @@
 (ns jepsen-xa.client
   (:require [integrant.core :as ig]
             [jepsen.tests :as tests]
+            [jepsen-xa.log :as log]
             [jepsen.cli :as cli]))
 
 (defn load-config
@@ -9,13 +10,24 @@
     {:keys [app other]} :logger}]
   {:jepsen-xa.boundary.log/level {:app app :other other}
    :jepsen-xa.spec/instrument {:enable instrument
-                               :logger (ig/ref :jepsen-xa.boundary.log/level)}})
+                               :logger (ig/ref :jepsen-xa.boundary.log/level)}
+   :jepsen-xa.client/test-fn nil
+   :jepsen-xa.client/runner {:test-fn (ig/ref :jepsen-xa.client/test-fn)
+                             :logger (ig/ref :jepsen-xa.boundary.log/level)}})
 
 (defn make-xa-test
-  []
+  [opts]
   (merge tests/noop-test
-         {:pure-generators true}))
+         {:pure-generators true}
+         opts))
 
+(defmethod ig/init-key ::test-fn [_ _]
+  make-xa-test)
+
+(defmethod ig/init-key ::runner [_ {:keys [test-fn logger]}]
+  (log/debug logger {:test-fn test-fn})
+  #(cli/run!
+    (cli/single-test-cmd {:test-fn test-fn}) ["test"]))
 
 (defn -main
   [& args]
--- jepsen-xa/test/jepsen_xa/client_test.clj
+++ jepsen-xa/test/jepsen_xa/client_test.clj
@@ -6,10 +6,12 @@
 
 (deftest test-load-config
   (testing "This configuration can be used in REPL."
-    (is (=   {:jepsen-xa.boundary.log/level {:app :debug :other :info}
-              :jepsen-xa.spec/instrument {:enable true
-                                          :logger (ig/ref :jepsen-xa.boundary.log/level)}}
-             
+    (is (= {:jepsen-xa.boundary.log/level {:app :debug :other :info}
+            :jepsen-xa.spec/instrument {:enable true
+                                        :logger (ig/ref :jepsen-xa.boundary.log/level)}
+            :jepsen-xa.client/test-fn nil
+            :jepsen-xa.client/runner {:test-fn (ig/ref :jepsen-xa.client/test-fn)
+                                      :logger (ig/ref :jepsen-xa.boundary.log/level)}}
            (load-config {:logger {:app :debug :other :info}
                          :instrument true
                          :db-specs {:db1 {:port 55432 :host "127.0.0.1"}
